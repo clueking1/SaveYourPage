f065344fa0984abcea0d00fb5c375eb737b9d8f1
clueking1 <wcoan300@gmail.com>
2020-04-19 22:31:57 -0400

    added media wrapper to display search results

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navi from '../../component/Nav'
 import Search from '../../component/SearchInput'
+import SearchMedia from '../../component/searchMedia'
 import './style.css'
 
 function Home() {
@@ -12,6 +13,10 @@ function Home() {
                 <h2>Find Your Book</h2>
                 <p>Search for a book and save it to your library!</p>
                 <Search />
+            </div>
+
+            <div className="searchedWrapper">
+                <SearchMedia />
             </div>
             
         </div>    
