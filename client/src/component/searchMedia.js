bd0a3745fb4c6122229d600e7f66348627210ad6
clueking1 <wcoan300@gmail.com>
2020-04-20 12:24:13 -0400

    added overlay to access more info about books that peaks the interest of the user

--- client/src/component/searchMedia.js
+++ client/src/component/searchMedia.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Media } from 'reactstrap';
+
 
 
 const searchMedia = () => {
+
+  function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
   return (
-    <Media>
-     <Media left href="#">
-        <Media object data-src="holder.js/64x64" alt="Generic placeholder image" />
-      </Media>
-      <Media body>
-        <Media heading>
-          Media heading
-        </Media>
-        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-      </Media>
-    </Media>
+     <div className="nestedWrapper">
+      
+      <div className="image">
+        <img src="https://via.placeholder.com/300" alt="img1"/>
+      </div>
+      <span onClick={openNav}>View More</span>
+      {/* <div className="description">
+        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+      </div> */}
+          <div id="myNav" class="overlay">
+      <a href="javascript:void(0)" className="closebtn" onClick={closeNav} >&times;</a>
+        <div class="overlay-content">
+            <div className="bookInfo">
+          <h3>Harry Potter</h3>
+          <p>The Stones Of Misery</p>
+          <p>Author: <i>William</i></p>
+        </div>
+        <div className="buttonGroup">
+          <a href='/'>View</a>
+          <a href='/'>Save</a>
+        </div>
+          <div className="description">
+            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+          </div>
+        </div>
+      </div>
+     </div>
   );
 };
 
