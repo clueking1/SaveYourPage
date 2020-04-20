6aed7f017818733df4cf382bdfaf3a7f2cbcc218
clueking1 <wcoan300@gmail.com>
2020-04-19 20:31:13 -0400

    mongodb database with mongoose connected

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
-const routes = require('./routes')
+//const routes = require('./routes')
 const app = express()
 
 const PORT = process.env.PORT || 3001
@@ -12,9 +12,16 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
 } 
 
-app.use(routes)
+//app.use(routes)
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/saveyourpage')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/saveyourpage', 
+{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
+const connection = mongoose.connection
+
+connection.once('open', () => {
+    console.log('MongoDB database connected')
+})
 
 app.listen(PORT, function() {
     console.log(`API running on port: ${PORT}`)
