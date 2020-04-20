7eb59fb4dae8c8ee36abbeba8d07558c74ceb5e1
clueking1 <wcoan300@gmail.com>
2020-04-20 15:13:49 -0400

    able to click on a book from the searched results and see the needed info

--- client/src/component/searchMedia.js
+++ client/src/component/searchMedia.js
@@ -1,26 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css"
 import API from '../utils/API'
-import { CardImgOverlay } from 'reactstrap';
+import { CardImgOverlay, CardTitle } from 'reactstrap';
 
 
 
-const searchMedia = (props) => {
-  console.log(props.data.items)
+const SearchMedia = (props) => {
+  const [over, setOver] = useState({
+    id: '',
+    title: '',
+    author: '',
+    snip: ''
+  })
 
   function openNav(id) {
     document.getElementById("myNav").style.width = "100%";
-   overlay(id)
+    console.log(over)
   }
   
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
+    //console.log(over)
   }
 
   function overlay(id) {
     API.overlayBook(id)
       .then(res => {
-        console.log(res)
+        console.log(res.data.id)
+        setOver({
+          id: res.data.id,
+          title: res.data.volumeInfo.title,
+          author: res.data.volumeInfo.authors[0],
+          snip: res.data.volumeInfo.description
+        })
+      })
+      .then(() => {
+        openNav()
       })
   }
 
@@ -32,7 +47,7 @@ const searchMedia = (props) => {
                 <div className="image">
                 <img src={t.volumeInfo.imageLinks.thumbnail} alt="img1"/>
               </div>
-              <span onClick={() => openNav(t.id) }>View More</span>
+              <span onClick={() => overlay(t.id) }>View More</span>
           </div>
          ))} 
             <div id="myNav" className="overlay">
@@ -40,9 +55,9 @@ const searchMedia = (props) => {
                 <div className="overlay-content">
                   <div className="overlaySearched">
                     <div className="bookInfo">
-                      <h3></h3>
+                      <h3>{over.title}</h3>
                       {/* <p>{item.volumeInfo.authors[0]}</p> */}
-                      <p>Author: <i></i></p>
+                      <p>Author: <i>{over.author}</i></p>
                     </div>
                     <div className="buttonGroup">
                       <a href='/'>View</a>
@@ -50,7 +65,7 @@ const searchMedia = (props) => {
                     </div>
                 
                     <div className="description">
-                      <p></p>
+                      <p>{over.snip}</p>
                     </div>
                   </div> 
                 </div>
@@ -63,4 +78,4 @@ const searchMedia = (props) => {
   );
 };
 
-export default searchMedia;
+export default SearchMedia;
