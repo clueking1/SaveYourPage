bd5f42738d4e000af1bb203da5d6e68f8c43245d
clueking1 <wcoan300@gmail.com>
2020-04-20 12:35:11 -0400

    fixed overlay container

--- client/src/component/searchMedia.js
+++ client/src/component/searchMedia.js
@@ -14,33 +14,35 @@ const searchMedia = () => {
   }
 
   return (
-     <div className="nestedWrapper">
-      
-      <div className="image">
-        <img src="https://via.placeholder.com/300" alt="img1"/>
-      </div>
-      <span onClick={openNav}>View More</span>
-      {/* <div className="description">
-        <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-      </div> */}
-          <div id="myNav" class="overlay">
-      <a href="javascript:void(0)" className="closebtn" onClick={closeNav} >&times;</a>
-        <div class="overlay-content">
-            <div className="bookInfo">
-          <h3>Harry Potter</h3>
-          <p>The Stones Of Misery</p>
-          <p>Author: <i>William</i></p>
+    <div className="nestedWrapper">
+        
+        <div className="image">
+          <img src="https://via.placeholder.com/300" alt="img1"/>
         </div>
-        <div className="buttonGroup">
-          <a href='/'>View</a>
-          <a href='/'>Save</a>
-        </div>
-          <div className="description">
-            <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+        <span onClick={openNav}>View More</span>
+
+      <div id="myNav" className="overlay">
+              <a href="javascript:void(0)" className="closebtn" onClick={closeNav} >&times;</a>
+          <div class="overlay-content">
+            <div className="overlaySearched">
+              <div className="bookInfo">
+                <h3>Harry Potter</h3>
+                <p>The Stones Of Misery</p>
+                <p>Author: <i>William</i></p>
+              </div>
+              <div className="buttonGroup">
+                <a href='/'>View</a>
+                <a href='/'>Save</a>
+              </div>
+          
+              <div className="description">
+                <p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
+              </div>
+            </div> 
           </div>
-        </div>
       </div>
-     </div>
+    </div>
+      
   );
 };
 
